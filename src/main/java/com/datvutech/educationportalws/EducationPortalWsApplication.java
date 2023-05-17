03b0a1d8ff536202202476953c1ed4f388c56077
vutiendat3601 <vutien.dat.3601@gmail.com>
2023-05-17 18:14:09 +0700

    initial

---
+++ src/main/java/com/datvutech/educationportalws/EducationPortalWsApplication.java
@@ -0,0 +1,13 @@
+package com.datvutech.educationportalws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationPortalWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducationPortalWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/datvutech/educationportalws/EducationPortalWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datvutech.educationportalws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationPortalWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
