cf44b55fd155f82762d0abbc3eb998d5ac8f31bc
vutiendat3601 <vutien.dat.3601@gmail.com>
2023-05-18 22:08:13 +0700

    config Security

--- src/main/java/com/datvutech/educationportalws/security/SecurityConfig.java
+++ src/main/java/com/datvutech/educationportalws/security/SecurityConfig.java
@@ -8,9 +8,19 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.datvutech.educationportalws.jwt.JwtAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthFilter;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthFilter) {
+        this.jwtAuthFilter = jwtAuthFilter;
+    }
+
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -25,10 +35,12 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http
-        .csrf(csrf -> csrf.disable())
-        .authorizeHttpRequests(
-                req -> req.anyRequest()
-                        .permitAll());
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(
+                        req -> req
+                                .antMatchers("/users/**").hasRole("ADMIN")
+                                .antMatchers("/login").permitAll())
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 }
